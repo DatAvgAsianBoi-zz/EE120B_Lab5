0a6a3f5e13d1ac7f7fa2890640aeaf1f29398773
Ryan Hu <huryan18@gmail.com>
2020-04-26 00:27:13 -0700

    part 1 complete

--- turnin/rhu017_lab3_part1.c
+++ turnin/rhu017_lab3_part1.c
@@ -6,6 +6,8 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+
+ *    Video Link: https://youtu.be/5v02JYH57Es
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
